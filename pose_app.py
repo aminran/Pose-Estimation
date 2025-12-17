7e33746a231bd88672de3deaa364dbf5327b175c
amin <ranjbaran_amin@yahoo.com>
2025-12-17 12:20:30 +0330

    change font and size on output video, clean up notebook

--- pose_app.py
+++ pose_app.py
@@ -36,7 +36,7 @@ if mode == "Upload Video":
 # -------------------------
 # Live Camera
 # -------------------------
-else:
+if mode == "Live Camera":
     cap = cv2.VideoCapture(0)  # 0 = default camera
 
 # -------------------------
@@ -49,78 +49,78 @@ frame_placeholder = st.empty()
 # -------------------------
 counter = 0
 stage = None
+if mode != None :
+    with mp_pose.Pose(min_detection_confidence=0.5,
+                    min_tracking_confidence=0.5) as pose:
 
-with mp_pose.Pose(min_detection_confidence=0.5,
-                  min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
+            # Convert to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            results = pose.process(image)
 
-        # Convert to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        results = pose.process(image)
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            try:
+                landmarks = results.pose_landmarks.landmark
+                hip = landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value]
+                shoulder = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
 
-        try:
-            landmarks = results.pose_landmarks.landmark
-            hip = landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value]
-            shoulder = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value]
+                angle = calculate_angle(hip, shoulder)
 
-            angle = calculate_angle(hip, shoulder)
+                h, w, _ = image.shape
+                cv2.putText(
+                    image,
+                    str(angle),
+                    tuple(np.multiply((hip.x, hip.y), [w, h]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA
+                )
 
+                # Counting logic
+                if angle < 7:
+                    stage = "down"
+                if angle > 40 and stage == "down":
+                    stage = "up"
+                    counter += 1
+
+            except:
+                pass
+
+            # UI overlay
             h, w, _ = image.shape
-            cv2.putText(
+            cv2.rectangle(image, (0, 0), (200, 80), (255, 100, 0), -1)
+            cv2.putText(image, "REPS:", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            cv2.putText(image, str(counter), (100, 65),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
+            cv2.rectangle(image, (w - 200, 0), (w, 80), (255, 100, 0), -1)
+            cv2.putText(image, "Stage:", (w - 190, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            cv2.putText(image, str(stage), (w - 90, 65),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
+            mp_drawing.draw_landmarks(
                 image,
-                str(angle),
-                tuple(np.multiply((hip.x, hip.y), [w, h]).astype(int)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS
             )
 
-            # Counting logic
-            if angle < 7:
-                stage = "down"
-            if angle > 40 and stage == "down":
-                stage = "up"
-                counter += 1
+            # Convert BGR to RGB for Streamlit
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frame_placeholder.image(image_rgb, channels="RGB")
 
-        except:
-            pass
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        # UI overlay
-        h, w, _ = image.shape
-        cv2.rectangle(image, (0, 0), (200, 80), (255, 100, 0), -1)
-        cv2.putText(image, "REPS", (10, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
-        cv2.putText(image, str(counter), (10, 65),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.8, (0, 255, 255), 2)
-
-        cv2.rectangle(image, (w - 200, 0), (w, 80), (255, 100, 0), -1)
-        cv2.putText(image, "Stage", (w - 190, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
-        cv2.putText(image, str(stage), (w - 190, 65),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.8, (0, 255, 255), 2)
-
-        mp_drawing.draw_landmarks(
-            image,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS
-        )
-
-        # Convert BGR to RGB for Streamlit
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        frame_placeholder.image(image_rgb, channels="RGB")
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
